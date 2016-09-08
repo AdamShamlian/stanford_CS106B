a2cd82691568d986f02c4c38d908e27664532af4
Adam Shamlian <adam.shamlian@gmail.com>
2016-09-08 00:22:18 -0500

    Debugged boggle_recurser (removed wordLength and adjusted accordingly), Debugged verify_recurser()

--- hw4/BoggleMini/src/bogglesearch.cpp
+++ hw4/BoggleMini/src/bogglesearch.cpp
@@ -1,8 +1,13 @@
-// This is the .cpp file you will edit and turn in.
-// We have provided a skeleton for you,
-// but you must finish it as described in the spec.
-// Also remove these comments here and add your own.
-// TODO: remove this comment header
+/* The BoggleMini game! Implemented via recursive
+ * backtracking. The two assigned functions do some
+ * preprocessing and then pass off the rest to the
+ * recursive workhorses, verify_recurser and boggle_recurser.
+ * Both recursive functions implement a very similar
+ * conceptual architecture, but I elected not to refactor
+ * this because I think doing so would have ended up in
+ * so many little functions that it would have only
+ * obfuscated the code.
+ */
 
 // Please feel free to add any other #includes you need!
 #include <string>
@@ -21,19 +26,29 @@ using namespace std;
 bool verify_recurser(Grid<char>& board, string word, Set<Vector<int>>& usedLocations,
                      int curRow, int curCol) {
 
-    // negative base csae when we dont find a letter anywhere nearby
-    if (!board.get(curRow, curCol) == word[0]) {
-        return false;
+    // positive base case
+    if ((word.length() == 1) && (board.get(curRow, curCol) == word.at(0))) {
+        return true;
     }
 
-    // positive basecase when a single char is left and nearby
-    if ((word.length() == 1) && (board.get(curRow, curCol) == word[0])) {
-        return true;
+    /* DEAR WORLD,
+     *
+     * (!board.get(curRow, curCol) == word.at(0))
+     * IS NOT - I REPEAT, IS NOT - THE SAME AS
+     * (!(board.get(curRow, curCol) == word.at(0)))
+     * HOLY CRAP, WHAT A BUG! T_T.
+     *
+     *                        SINCERELY,
+     *                        NOOB
+     */
+    // negative base case
+    if (!(board.get(curRow, curCol) == word.at(0))) {
+        return false;
     }
 
     // loop through the possible directions we can go
     int newRow, newCol;
-    Vector<int> loc;
+    Vector<int> loc = Vector<int>();
     bool res;
 
     for (int tmpCol = -1; tmpCol < 2; tmpCol++) {
@@ -44,10 +59,11 @@ bool verify_recurser(Grid<char>& board, string word, Set<Vector<int>>& usedLocat
             // these were declared above, outside of the loop
             newRow = curRow + tmpRow;
             newCol = curCol + tmpCol;
-            loc = Vector<int>();
+            loc.clear();
             loc += newRow, newCol;
 
             // pathological checks to ignore: same place, oob, already used
+            // 1 and 3 are redundant, no?
             if ((newRow == curRow) & (newCol == curCol)) {
                 continue;
             }
@@ -60,7 +76,7 @@ bool verify_recurser(Grid<char>& board, string word, Set<Vector<int>>& usedLocat
             else {
                 // the secret sauce is here!
                 usedLocations.add(loc);
-                res = verify_recurser(board, word.substr(1, word.length()), usedLocations,
+                res = verify_recurser(board, word.substr(1, word.length() - 1), usedLocations,
                                 newRow, newCol);
                 if (res) { return true; } // if we verify the word, stop the search
                 usedLocations.remove(loc);
@@ -79,9 +95,13 @@ bool verify_recurser(Grid<char>& board, string word, Set<Vector<int>>& usedLocat
  */
 bool humanWordSearch(Grid<char>& board, string word) {
 
+    // automatically filter out "cheating"
+    if (word.length() < MIN_WORD_LENGTH) {
+        return false;
+    }
+
     // we know the board uses upper case only
     toUpperCaseInPlace(word);
-
     Set<Vector<int>> usedLocations = Set<Vector<int>>();
 
     for (int curCol = 0; curCol < board.numCols(); curCol++) {
@@ -102,7 +122,8 @@ bool humanWordSearch(Grid<char>& board, string word) {
  */
 bool boggle_recurser(Set<string>& foundWords, Grid<char>& board, Lexicon& dictionary,
                      string word, Set<Vector<int>>& usedLocations,
-                     int curRow, int curCol, int wordLength) {
+                     int curRow, int curCol) {
+
 
     // prune the recursive tree
     if (!dictionary.containsPrefix(toLowerCase(word))){
@@ -110,18 +131,15 @@ bool boggle_recurser(Set<string>& foundWords, Grid<char>& board, Lexicon& dictio
     }
 
     // base case, const provided in boggle.h
-    if (wordLength >= MIN_WORD_LENGTH) {
+    if ((word.length() >= MIN_WORD_LENGTH) && (!foundWords.contains(word))) {
 
         // dict only has lowercase words, btw
         if (dictionary.contains(toLowerCase(word))) {
             foundWords.add(word);
             // gotta keep going for longer length words
             boggle_recurser(foundWords, board, dictionary,
-                            word, usedLocations, curRow, curCol, wordLength);
+                            word, usedLocations, curRow, curCol);
             return true;
-        }
-        else {
-            return false;
         }
     }
 
@@ -141,6 +159,8 @@ bool boggle_recurser(Set<string>& foundWords, Grid<char>& board, Lexicon& dictio
             loc = Vector<int>();
             loc += newRow, newCol;
 
+            // same as in verification
+            // 1 and 3 are redundant
             if ((newRow == curRow) & (newCol == curCol)) {
                 continue;
             }
@@ -154,8 +174,8 @@ bool boggle_recurser(Set<string>& foundWords, Grid<char>& board, Lexicon& dictio
                 // the secret sauce is here!
                 usedLocations.add(loc);
                 word += board.get(newRow, newCol);
-                res = boggle_recurser(foundWords, board, dictionary, word, usedLocations,
-                                newRow, newCol, wordLength - 1);
+                res = boggle_recurser(foundWords, board, dictionary, word,
+                                      usedLocations, newRow, newCol);
                 word.pop_back();
                 usedLocations.remove(loc);
             }
@@ -178,7 +198,7 @@ Set<string> computerWordSearch(Grid<char>& board, Lexicon& dictionary) {
     for (int curCol = 0; curCol < board.numCols(); curCol++) {
         for (int curRow = 0; curRow < board.numRows(); curRow++) {
             boggle_recurser(allWords, board, dictionary, word, usedLocs,
-                            curRow, curCol, 4);
+                            curRow, curCol);
         }
     }
     return allWords;
